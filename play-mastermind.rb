368ce5d140fbcddd54af47fe13875fa056c0da3b
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2019-11-11 20:06:56 -0500

    refactor #match through creation of #insert_black_peg and #insert_white_peg

--- play-mastermind.rb
+++ play-mastermind.rb
@@ -6,34 +6,89 @@ class Mastermind
     @codemaker = Codemaker.new
     @codebreaker = Codebreaker.new
     #@code_to_break = @codemaker.create_code
-    @code_to_break = ["purple", "purple", "yellow", "purple"]
-    @peg_array = []
+    @code_to_break = ["yellow", "orange", "yellow", "purple"]
+    @code_copy = []
+    @peg_array = [] #maybe delete this if insert black peg breaks
+    @white_peg_values = [] #maybe delete this if insert black peg breaks
+    @game_over = false
+    @total_guesses = 0
+    @guess = []
   end
 
   def match
-    code_copy = @code_to_break
-    #peg_array = []
-    white_peg_values = []
-    p code_copy
+    @code_copy = @code_to_break
+    @peg_array = [] 
+    @white_peg_values = []
+    p @code_copy
+    insert_black_pegs
+    insert_white_pegs
+=begin
     guess = @codebreaker.guess
-    code_copy.each_with_index do |color, index|
-      #p "#{color}: #{index}"
-      if code_copy[index] == guess[index]
+    @code_copy.each_with_index do |color, index|
+      if @code_copy[index] == guess[index]
         @peg_array.push("black_peg")
-        code_copy[index] = "removed"
+        @code_copy[index] = "removed"
         guess[index] = "removed"
-      elsif code_copy[index] != guess[index] && guess.include?(color)
+      elsif @code_copy[index] != guess[index] && guess.include?(color)
         white_peg_values.push(color)
         white_peg_values = white_peg_values.uniq
       end
     end
-    white_peg_values.each { |color| @peg_array.push("white_peg") }
-    # code_copy = @code_to_break (I don't think I need this line of code because 
+=end
+    @white_peg_values.each { |color| @peg_array.push("white_peg") }
+    # @code_copy = @code_to_break (I don't think I need this line of code because 
     #the "removed" elements will be replaced with their proper terms in line 13)
     p @peg_array
   end
+
+  def insert_pegs
+    #(@guess for below)?
+    guess = @codebreaker.guess
+    @code_copy.each_with_index do |color, index|
+      #p "#{color}: #{index}"
+      if @code_copy[index] == guess[index]
+        @peg_array.push("black_peg")
+        @code_copy[index] = "removed"
+        guess[index] = "removed"
+      elsif @code_copy[index] != guess[index] && guess.include?(color)
+        @white_peg_values.push(color)
+        @white_peg_values = @white_peg_values.uniq
+      end
+    end
+  end
+
+  def insert_black_pegs
+    @guess = @codebreaker.guess
+    @code_copy.each_with_index do |color, index|
+      #p "#{color}: #{index}"
+      if @code_copy[index] == @guess[index]
+        @peg_array.push("black_peg")
+        @code_copy[index] = "removed"
+        @guess[index] = "removed"
+      end
+    end
+    @peg_array
+  end
+
+  def insert_white_pegs
+    @code_copy.each_with_index do |color, index|
+      next if @code_copy[index] == "removed"
+      if @code_copy[index] != @guess[index] && @guess.include?(color)
+        @white_peg_values.push(color)
+        @white_peg_values = @white_peg_values.uniq
+      end
+    end
+  end
   
-  
+  def game_over?
+    if (@peg_array == ["black_peg", "black_peg", "black_peg", "black_peg"] || #I may be able to refactor this as peg_array[3] == "black_peg"
+      total_guesses > 12)
+      @game_over = true
+      return true
+    else
+      return false
+    end
+  end
 
 end #class end
 
@@ -51,6 +106,12 @@ game.match
 #my guess:      ["purple", "yellow", "purple", "blue"]
 #actual result: ["black_peg", "white_peg", "white_peg", "white_peg"]
 #expect result: [black_peg, white_peg, white_peg]
+
+#comp code: ["purple", "purple", "yellow", "purple"]
+#guess:     ["green", "red", "red", "purple"]
+#actual result: ["black_peg", "white_peg"]
+#expected result: [black peg]
+
 
 
 =begin
@@ -77,5 +138,28 @@ tests above
   end
 
 
+---the below works with white beg
+    def match
+    @code_copy = @code_to_break
+    #peg_array = [] remove comment if breaks to revert
+    #white_peg_values = [] remove comment if breaks to revert
+    p @code_copy
+    guess = @codebreaker.guess
+    @code_copy.each_with_index do |color, index|
+      #p "#{color}: #{index}"
+      if @code_copy[index] == guess[index]
+        @peg_array.push("black_peg")
+        @code_copy[index] = "removed"
+        guess[index] = "removed"
+      elsif @code_copy[index] != guess[index] && guess.include?(color)
+        white_peg_values.push(color)
+        white_peg_values = white_peg_values.uniq
+      end
+    end
+    white_peg_values.each { |color| @peg_array.push("white_peg") }
+    # @code_copy = @code_to_break (I don't think I need this line of code because 
+    #the "removed" elements will be replaced with their proper terms in line 13)
+    p @peg_array
+  end
 
 =end
