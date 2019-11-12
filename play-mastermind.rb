4075788bc9dd0aea3c13242f05b2359e63074c9c
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2019-11-11 19:09:40 -0500

    add white_peg indication for #match in Mastermind class

--- play-mastermind.rb
+++ play-mastermind.rb
@@ -5,34 +5,73 @@ class Mastermind
   def initialize
     @codemaker = Codemaker.new
     @codebreaker = Codebreaker.new
-    @code_to_break = @codemaker.create_code
+    #@code_to_break = @codemaker.create_code
+    @code_to_break = ["purple", "purple", "yellow", "purple"]
   end
 
   def match
+    code_copy = @code_to_break
     peg_array = []
+    white_peg_values = []
+    p code_copy
+    guess = @codebreaker.guess
+    code_copy.each_with_index do |color, index|
+      #p "#{color}: #{index}"
+      if code_copy[index] == guess[index]
+        peg_array.push("black_peg")
+        code_copy[index] = "removed"
+        guess[index] = "removed"
+      elsif code_copy[index] != guess[index] && guess.include?(color)
+        white_peg_values.push(color)
+        white_peg_values = white_peg_values.uniq
+      end
+    end
+    white_peg_values.each { |color| peg_array.push("white_peg") }
+    # code_copy = @code_to_break (I don't think I need this line of code because 
+    #the "removed" elements will be replaced with their proper terms in line 13)
+    p peg_array
+  end
+end #class end
+
+game = Mastermind.new
+game.match
+
+
+# TESTS
+#["yellow", "orange", "yellow", "purple"]
+#["yellow", "blue", "blue", "blue"]
+#expect: black_peg
+#result: black_peg, white_peg
+
+#computer code: ["purple", "purple", "yellow", "purple"]
+#my guess:      ["purple", "yellow", "purple", "blue"]
+#actual result: ["black_peg", "white_peg", "white_peg", "white_peg"]
+#expect result: [black_peg, white_peg, white_peg]
+
+
+=begin
+
+SAVE THIS..The below returns accurate black_peg, but doesn't pass the white peg
+tests above
+
+  def match
+    peg_array = []
+    white_peg_values = []
     p @code_to_break
     guess = @codebreaker.guess
     @code_to_break.each_with_index do |color, index|
       p "#{color}: #{index}"
       if @code_to_break[index] == guess[index]
         peg_array.push("black_peg")
+      elsif @code_to_break[index] != guess[index] && guess.include?(color)
+        white_peg_values.push(color)
+        white_peg_values = white_peg_values.uniq
       end
     end
+    white_peg_values.each { |color| peg_array.push("white_peg") }
     p peg_array
   end
-      
-=begin
-      if guess.index(color) != nil
-        peg_array.clear
-        peg_array.push "black_peg"
-      elsif guess.index(color) == nil && guess.include?(color)
-        peg_array.push("white_peg")
-      end  # if this returns something != nil we have a black peg (BUT if repeat colors were chosen we'd accidentally give two black pegs instead of 1)
-      p guess.include?(color) # white peg if true
-    end
-=end 
 
-end #class end
 
-game = Mastermind.new
-game.match
+
+=end
